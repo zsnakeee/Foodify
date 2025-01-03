fa83e273a4ee0c8e76472f96785bcd12560dcfb9
Ziad Talaat <ziadsnake33@gmail.com>
2025-01-03 12:54:24 +0200

    Delete .old/theme2 directory

--- .old/theme2/resources/views/components/logo.blade.php
+++
@@ -1,8 +0,0 @@
-@props([
-    'width' => null,
-    'height' => null,
-])
-
-<img src="{{ asset('assets/images/logo/logo-main.png') }}" alt="{{ config('app.name') }}"
-     width="{{ $width }}"
-     height="{{ $height }}" {{ $attributes }}>
