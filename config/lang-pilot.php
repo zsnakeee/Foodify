7ba66bebf846c055c1bf0acea1cb880607a3613a
Ziad Talaat <ziadsnake33@gmail.com>
2025-01-12 10:55:50 +0200

    lang-pilot package

---
+++ config/lang-pilot.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    /*
+     * The path to the language files.
+     */
+    'lang_path' => base_path('lang'),
+
+    /*
+     * The default driver to use.
+     */
+    'default' => \LangPilot\Drivers\GeminiTranslator::class,
+
+    /*
+     * The description of your app to asset in the translation process.
+     */
+    'description' => 'an ecommerce platform',
+
+    /*
+     * The driver's configuration.
+     */
+    'gemini' => [
+        'api_key' => env('GEMINI_API_KEY'),
+    ],
+];
