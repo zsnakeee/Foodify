0b135829d4adbd5716c6218bedfa8ee6298773fd
Ziad Talaat <ziadsnake33@gmail.com>
2025-01-17 22:00:53 +0200

    feat: add helper functions format_phone, slug and ui_avatar

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -6,3 +6,39 @@ if (! function_exists('format_price')) {
         return Number::currency($price, config('app.currency'), app()->getLocale());
     }
 }
+
+if (! function_exists('format_phone')) {
+    function format_phone($phone): string
+    {
+        try {
+            $phone = phone($phone)->formatInternational();
+        } catch (Exception $e) {
+        }
+
+        return $phone;
+    }
+}
+
+if (! function_exists('slug')) {
+    function slug($string, $separator = '-'): array|string|null
+    {
+        if (is_null($string)) {
+            return '';
+        }
+
+        $string = trim($string);
+        $string = mb_strtolower($string, 'UTF-8');
+        $string = preg_replace("/[^a-z0-9_\sءاأإآؤئبتثجحخدذرزسشصضطظعغفقكلمنهويةى]/u", '', $string);
+        $string = preg_replace("/[\s-]+/", ' ', $string);
+        $string = preg_replace("/[\s_]/", $separator, $string);
+
+        return $string;
+    }
+}
+
+if (! function_exists('ui_avatar')) {
+    function ui_avatar($name, $color = '7F9CF5', $background = 'EBF4FF'): string
+    {
+        return 'https://ui-avatars.com/api/?name='.urlencode($name).'&color='.$color.'&background='.$background;
+    }
+}
