e2ea2ae75860e4a322791a2922bd5afee0e7334a
Ziad Talaat <ziadsnake33@gmail.com>
2025-01-17 22:08:34 +0200

    feat(UserResource): add user resource to dashbaord

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use App\Traits\HasLocalizedLabel;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Support\HtmlString;
+
+class UserResource extends Resource
+{
+    use HasLocalizedLabel;
+
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-s-users';
+
+    protected static ?string $navigationGroup = 'Users';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required(),
+                Forms\Components\DateTimePicker::make('email_verified_at'),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required(),
+                Forms\Components\TextInput::make('avatar'),
+                Forms\Components\TextInput::make('phone')
+                    ->tel(),
+                Forms\Components\TextInput::make('role_id')
+                    ->numeric(),
+                Forms\Components\TextInput::make('oauth_provider'),
+                Forms\Components\TextInput::make('oauth_id'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->translateLabel()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('avatar')
+                    ->circular()
+                    ->defaultImageUrl(fn ($record) => $record->avatar),
+                Tables\Columns\TextColumn::make('phone')
+                    ->formatStateUsing(function (string $state) {
+                        $formatted = format_phone($state);
+
+                        return new HtmlString("<span style='direction: ltr; unicode-bidi: embed;'>$formatted</span>");
+                    })
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('oauth_provider')
+                    ->label('OAuth Provider')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('oauth_id')
+                    ->label('OAuth ID')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+               //
+           ])
+            ->actions([
+               Tables\Actions\EditAction::make(),
+           ])
+            ->bulkActions([
+               Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+               ]),
+           ]);
+
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
