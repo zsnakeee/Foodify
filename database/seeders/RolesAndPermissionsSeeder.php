2a2d6ba748b0669cc68a6c9aa61ae15804e568ce
Ziad Talaat <ziadsnake33@gmail.com>
2025-01-17 22:53:56 +0200

    chore: renamed permissions

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -14,69 +14,73 @@ class RolesAndPermissionsSeeder extends Seeder
         // Reset cached roles and permissions
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        Permission::create(['name' => 'product.access']);
+        Permission::create(['name' => 'product.view']);
         Permission::create(['name' => 'product.create']);
         Permission::create(['name' => 'product.edit']);
         Permission::create(['name' => 'product.delete']);
 
-        Permission::create(['name' => 'category.access']);
+        Permission::create(['name' => 'category.view']);
         Permission::create(['name' => 'category.create']);
         Permission::create(['name' => 'category.edit']);
         Permission::create(['name' => 'category.delete']);
 
-        Permission::create(['name' => 'brand.access']);
+        Permission::create(['name' => 'brand.view']);
         Permission::create(['name' => 'brand.create']);
         Permission::create(['name' => 'brand.edit']);
         Permission::create(['name' => 'brand.delete']);
 
-        Permission::create(['name' => 'promo.access']);
+        Permission::create(['name' => 'promo.view']);
         Permission::create(['name' => 'promo.create']);
         Permission::create(['name' => 'promo.edit']);
         Permission::create(['name' => 'promo.delete']);
 
-        Permission::create(['name' => 'order.access']);
+        Permission::create(['name' => 'order.view']);
+        Permission::create(['name' => 'order.create']);
         Permission::create(['name' => 'order.edit']);
         Permission::create(['name' => 'order.delete']);
 
-        Permission::create(['name' => 'user.access']);
+        Permission::create(['name' => 'user.view']);
+        Permission::create(['name' => 'user.create']);
         Permission::create(['name' => 'user.edit']);
         Permission::create(['name' => 'user.delete']);
 
-        Permission::create(['name' => 'role.access']);
+        Permission::create(['name' => 'role.view']);
         Permission::create(['name' => 'role.create']);
         Permission::create(['name' => 'role.edit']);
         Permission::create(['name' => 'role.delete']);
 
-        Permission::create(['name' => 'permission.access']);
+        Permission::create(['name' => 'permission.view']);
         Permission::create(['name' => 'permission.create']);
         Permission::create(['name' => 'permission.edit']);
         Permission::create(['name' => 'permission.delete']);
 
-        Permission::create(['name' => 'setting.access']);
+        Permission::create(['name' => 'setting.view']);
         Permission::create(['name' => 'setting.edit']);
 
         $manager = Role::create(['name' => 'manager']);
         $manager->givePermissionTo([
-            'product.access',
+            'product.view',
             'product.create',
             'product.edit',
             'product.delete',
-            'category.access',
+            'category.view',
             'category.create',
             'category.edit',
             'category.delete',
-            'brand.access',
+            'brand.view',
             'brand.create',
             'brand.edit',
             'brand.delete',
-            'promo.access',
+            'promo.view',
             'promo.create',
             'promo.edit',
             'promo.delete',
-            'order.access',
+            'order.view',
+            'order.create',
             'order.edit',
             'order.delete',
-            'user.access',
+            'user.view',
+            'user.create',
             'user.edit',
             'user.delete',
         ]);
