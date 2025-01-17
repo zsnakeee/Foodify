28603cbcc1cf3f23667113242b0e9352c11f83fa
Ziad Talaat <ziadsnake33@gmail.com>
2025-01-17 22:05:41 +0200

    feat(OrderDetail): add total attribute

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -23,5 +24,12 @@ class OrderDetail extends Model
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+
+    protected function total(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->quantity * $this->price,
+        );
     }
 }
