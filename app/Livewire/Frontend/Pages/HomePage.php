dbb0519e31761187b3ad04b16fd76b225fb68d75
Ziad Talaat <ziadsnake33@gmail.com>
2025-01-24 20:01:33 +0200

    chore: footer content and add newsletter signup.

--- app/Livewire/Frontend/Pages/HomePage.php
+++ app/Livewire/Frontend/Pages/HomePage.php
@@ -5,19 +5,46 @@ namespace App\Livewire\Frontend\Pages;
 use App\Models\Banner;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\Subscriber;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\On;
 use Livewire\Component;
+use Masmerise\Toaster\Toastable;
 
 #[Layout('layouts.app')]
 class HomePage extends Component
 {
+    use Toastable;
+
     public function render()
     {
         return view('livewire.frontend.pages.home-page', [
         ])->layoutData([
             'title' => __('Home'),
         ]);
+    }
+
+    #[On('subscribe')]
+    public function subscribe($email): void
+    {
+        $validatedData = Validator::make(['email' => $email], [
+            'email' => 'required|email',
+        ]);
+
+        if ($validatedData->fails()) {
+            $this->error($validatedData->errors()->first());
+
+            return;
+        }
+
+        Subscriber::createOrFirst([
+            'email' => $email,
+        ]);
+
+        $this->success(__('You have been subscribed successfully to our newsletter.'));
+        $this->dispatch('subscribed');
     }
 
     #[Computed]
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -16,136 +16,143 @@
                             </div>
                             <ul>
                                 <li>
-                                    <p>Address: 1234 Fashion Street, Suite 567, <br> New York, NY 10001</p>
+                                    <p>{{ __('Address') }}: {{ __('Alexandra, Egypt') }}</p>
                                 </li>
                                 <li>
-                                    <p>Email: <a href="#">info@fashionshop.com</a></p>
+                                    <p>{{ __('Email') }}: <a
+                                            href="mailto:{{ config('mail.from.address') }}">{{ config('mail.from.address') }}</a>
+                                    </p>
                                 </li>
                                 <li>
-                                    <p>Phone: <a href="#">(212) 555-1234</a></p>
+                                    <p>{{ __('Phone') }}: <a href="tel:{{ phone('+201127070346') }}"
+                                                             style="direction: ltr">{{ phone('+201127070346') }}</a></p>
                                 </li>
                             </ul>
-                            <a href="contact-1.html" class="tf-btn btn-line">Get direction<i class="icon icon-arrow1-top-left"></i></a>
+                            <a href="javascript:" class="tf-btn btn-line">{{ __('Get direction') }}<i
+                                    class="icon icon-arrow1-top-left"></i></a>
                             <ul class="tf-social-icon d-flex gap-10">
-                                <li><a href="#" class="box-icon w_34 round social-facebook social-line"><i class="icon fs-14 icon-fb"></i></a></li>
-                                <li><a href="#" class="box-icon w_34 round social-twiter social-line"><i class="icon fs-12 icon-Icon-x"></i></a></li>
-                                <li><a href="#" class="box-icon w_34 round social-instagram social-line"><i class="icon fs-14 icon-instagram"></i></a></li>
-                                <li><a href="#" class="box-icon w_34 round social-tiktok social-line"><i class="icon fs-14 icon-tiktok"></i></a></li>
-                                <li><a href="#" class="box-icon w_34 round social-pinterest social-line"><i class="icon fs-14 icon-pinterest-1"></i></a></li>
+                                <li><a href="#" class="box-icon w_34 round social-facebook social-line"><i
+                                            class="icon fs-14 icon-fb"></i></a></li>
+                                <li><a href="#" class="box-icon w_34 round social-twiter social-line"><i
+                                            class="icon fs-12 icon-Icon-x"></i></a></li>
+                                <li><a href="#" class="box-icon w_34 round social-instagram social-line"><i
+                                            class="icon fs-14 icon-instagram"></i></a></li>
+                                <li><a href="#" class="box-icon w_34 round social-tiktok social-line"><i
+                                            class="icon fs-14 icon-tiktok"></i></a></li>
+                                <li><a href="#" class="box-icon w_34 round social-pinterest social-line"><i
+                                            class="icon fs-14 icon-pinterest-1"></i></a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-xl-3 col-md-6 col-12 footer-col-block">
                         <div class="footer-heading footer-heading-desktop">
-                            <h6>Help</h6>
+                            <h6>{{ __('Help') }}</h6>
                         </div>
                         <div class="footer-heading footer-heading-moblie">
-                            <h6>Help</h6>
+                            <h6>{{ __('Help') }}</h6>
                         </div>
                         <ul class="footer-menu-list tf-collapse-content">
                             <li>
-                                <a href="privacy-policy.html" class="footer-menu_item">Privacy Policy</a>
+                                <a href="javascript:" class="footer-menu_item">{{ __('Privacy Policy') }}</a>
                             </li>
                             <li>
-                                <a href="delivery-return.html" class="footer-menu_item">  Returns + Exchanges </a>
+                                <a href="javascript:" class="footer-menu_item"> {{ __('Returns + Exchanges') }} </a>
                             </li>
                             <li>
-                                <a href="shipping-delivery.html" class="footer-menu_item">Shipping</a>
+                                <a href="javascript:" class="footer-menu_item">{{ __('Shipping') }}</a>
                             </li>
                             <li>
-                                <a href="terms-conditions.html" class="footer-menu_item">Terms &amp; Conditions</a>
+                                <a href="javascript:" class="footer-menu_item">{{ __('Terms and conditions') }}</a>
                             </li>
                             <li>
-                                <a href="faq-1.html" class="footer-menu_item">FAQ’s</a>
+                                <a href="javascript:" class="footer-menu_item">{{ __('FAQ’s') }}</a>
                             </li>
                             <li>
-                                <a href="compare.html" class="footer-menu_item">Compare</a>
+                                <a href="javascript:" class="footer-menu_item">{{ __('Compare') }}</a>
                             </li>
                             <li>
-                                <a href="wishlist.html" class="footer-menu_item">My Wishlist</a>
+                                <a href="javascript:" class="footer-menu_item">{{ __('My Wishlist') }}</a>
                             </li>
                         </ul>
                     </div>
                     <div class="col-xl-3 col-md-6 col-12 footer-col-block">
                         <div class="footer-heading footer-heading-desktop">
-                            <h6>About us</h6>
+                            <h6>{{ __('About us') }}</h6>
                         </div>
                         <div class="footer-heading footer-heading-moblie">
-                            <h6>About us</h6>
+                            <h6>{{ __('About us') }}</h6>
                         </div>
                         <ul class="footer-menu-list tf-collapse-content">
                             <li>
-                                <a href="about-us.html" class="footer-menu_item">Our Story</a>
+                                <a href="javascript:" class="footer-menu_item">{{ __('Our Story') }}</a>
                             </li>
                             <li>
-                                <a href="our-store.html" class="footer-menu_item">Visit Our Store</a>
+                                <a href="javascript:" class="footer-menu_item">{{ __('Visit Our Store') }}</a>
                             </li>
                             <li>
-                                <a href="contact-1.html" class="footer-menu_item">Contact Us</a>
+                                <a href="javascript:" class="footer-menu_item">{{ __('Contact Us') }}</a>
                             </li>
                             <li>
-                                <a href="login.html" class="footer-menu_item">Account</a>
+                                <a href="javascript:" class="footer-menu_item">{{ __('Account') }}</a>
                             </li>
                         </ul>
                     </div>
                     <div class="col-xl-3 col-md-6 col-12">
                         <div class="footer-newsletter footer-col-block">
                             <div class="footer-heading footer-heading-desktop">
-                                <h6>Sign Up for Email</h6>
+                                <h6>{{ __('Sign Up for Email') }}</h6>
                             </div>
                             <div class="footer-heading footer-heading-moblie">
-                                <h6>Sign Up for Email</h6>
+                                <h6>{{ __('Sign Up for Email') }}</h6>
                             </div>
                             <div class="tf-collapse-content">
-                                <div class="footer-menu_item">Sign up to get first dibs on new arrivals, sales, exclusive content, events and more!</div>
-                                <form class="form-newsletter" id="subscribe-form" action="#" method="post" accept-charset="utf-8" data-mailchimp="true">
-                                    <div id="subscribe-content">
-                                        <fieldset class="email">
-                                            <input type="email" name="email-form" id="subscribe-email" placeholder="Enter your email...." tabindex="0" aria-required="true">
-                                        </fieldset>
-                                        <div class="button-submit">
-                                            <button id="subscribe-button" class="tf-btn btn-sm radius-3 btn-fill btn-icon animate-hover-btn" type="button">Subscribe<i class="icon icon-arrow1-top-left"></i></button>
-                                        </div>
-                                    </div>
-                                    <div id="subscribe-msg"></div>
-                                </form>
-                                <div class="tf-cur">
-                                    <div class="tf-currencies">
-                                        <select class="image-select center style-default type-currencies">
-                                            <option data-thumbnail="{{ asset("assets/images/country/fr.svg") }}">EUR <span>€ | France</span></option>
-                                            <option data-thumbnail="{{ asset("assets/images/country/de.svg") }}">EUR <span>€ | Germany</span></option>
-                                            <option selected data-thumbnail="{{ asset("assets/images/country/us.svg") }}">USD <span>$ | United States</span></option>
-                                            <option data-thumbnail="{{ asset("assets/images/country/vn.svg") }}">VND <span>₫ | Vietnam</span></option>
-                                        </select>
-                                    </div>
-                                    <div class="tf-languages">
-                                        <select class="image-select center style-default type-languages">
-                                            <option>English</option>
-                                            <option>العربية</option>
-                                            <option>简体中文</option>
-                                            <option>اردو</option>
-                                        </select>
+                                <div
+                                    class="footer-menu_item"{{ __('Sign up for our newsletter and be the first to know about coupons and special promotions.') }}
+                            </div>
+                            <form class="form-newsletter" accept-charset="utf-8" data-mailchimp="true">
+                                <div x-data="{ email: '' }">
+                                    <fieldset class="email">
+                                        <input type="email"
+                                               x-model="email"
+                                               placeholder="{{ __('Enter your email....') }}"
+                                               tabindex="0"
+                                               aria-required="true">
+                                    </fieldset>
+                                    <div class="button-submit">
+                                        <button class="tf-btn btn-sm radius-3 btn-fill btn-icon animate-hover-btn"
+                                                x-on:click.prevent="$dispatch('subscribe', { email: email })"
+                                                x-on:keydown.enter.prevent="$dispatch('subscribe', { email: email })"
+                                                x-on-subscribed="email = ''"
+                                                type="button">{{ __('Subscribe') }}<i
+                                                class="icon icon-arrow1-top-left"></i>
+                                        </button>
                                     </div>
                                 </div>
-                            </div>
+                                <div id="subscribe-msg"></div>
+                            </form>
+
+                            <livewire:frontend.components.locale-switcher/>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="footer-bottom">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="footer-bottom-wrap d-flex gap-20 flex-wrap justify-content-between align-items-center">
-                            <div class="footer-menu_item">© 2024 Ecomus Store. All Rights Reserved</div>
-                            <div class="tf-payment">
-                                <img src="{{ asset("assets/images/payments/visa.png") }}" alt="">
-                                <img src="{{ asset("assets/images/payments/img-1.png") }}" alt="">
-                                <img src="{{ asset("assets/images/payments/img-2.png") }}" alt="">
-                                <img src="{{ asset("assets/images/payments/img-3.png") }}" alt="">
-                                <img src="{{ asset("assets/images/payments/img-4.png") }}" alt="">
-                            </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div
+                        class="footer-bottom-wrap d-flex gap-20 flex-wrap justify-content-between align-items-center">
+                        <div class="footer-menu_item">
+                            {{ __('© :year :name. All Rights Reserved.', ['year' => date('Y'), 'name' => config('app.name')]) }}
+                        </div>
+                        <div class="tf-payment">
+                            <img src="{{ asset("assets/images/payments/visa.png") }}" alt="">
+                            <img src="{{ asset("assets/images/payments/img-1.png") }}" alt="">
+                            <img src="{{ asset("assets/images/payments/img-2.png") }}" alt="">
+                            <img src="{{ asset("assets/images/payments/img-3.png") }}" alt="">
+                            <img src="{{ asset("assets/images/payments/img-4.png") }}" alt="">
                         </div>
                     </div>
                 </div>
